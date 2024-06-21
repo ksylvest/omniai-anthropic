d24896d6d993a1bf3666476b644c31ff7775dfed
Kevin Sylvestre <kevin@ksylvest.com>
2024-06-21 11:12:20 -0700

    Support logging / timeout

--- lib/omniai/anthropic/config.rb
+++ lib/omniai/anthropic/config.rb
@@ -2,16 +2,30 @@
 
 module OmniAI
   module Anthropic
-    # Config for the Anthropic `api_key` / `host` / `logger` / `version` / `chat_options`.
+    # Configuration for Anthropic.
     class Config < OmniAI::Config
-      attr_accessor :version, :chat_options
+      DEFAULT_HOST = 'https://api.anthropic.com'
+      DEFAULT_VERSION = '2023-06-01'
 
-      def initialize
-        super
-        @api_key = ENV.fetch('ANTHROPIC_API_KEY', nil)
-        @host = ENV.fetch('ANTHROPIC_HOST', 'https://api.anthropic.com')
-        @version = ENV.fetch('ANTHROPIC_VERSION', '2023-06-01')
-        @chat_options = { max_tokens: 4096 }
+      # @!attribute [rw] version
+      #   @return [String, nil] passed as `anthropic-version` if specified
+      attr_accessor :version
+
+      # @param api_key [String, nil] optional - defaults to `ENV['ANTHROPIC_API_KEY']`
+      # @param host [String, nil] optional - defaults to `ENV['ANTHROPIC_HOST'] w/ fallback to `DEFAULT_HOST`
+      # @param version [String, nil] optional - defaults to `ENV['ANTHROPIC_VERSION'] w/ fallback to `DEFAULT_VERSION`
+      # @param logger [Logger, nil] optional - defaults to
+      # @param timeout [Integer, Hash, nil] optional
+      def initialize(
+        api_key: ENV.fetch('ANTHROPIC_API_KEY', nil),
+        host: ENV.fetch('ANTHROPIC_HOST', DEFAULT_HOST),
+        version: ENV.fetch('ANTHROPIC_VERSION', DEFAULT_VERSION),
+        logger: nil,
+        timeout: nil
+      )
+        super(api_key:, host:, logger:, timeout:)
+        @version = version
+        @chat_options[:max_tokens] = 4096
       end
     end
   end
