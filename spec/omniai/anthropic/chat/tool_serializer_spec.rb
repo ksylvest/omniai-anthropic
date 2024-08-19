ffbad3db55cb7de2c0342e1319e23ceac7d9dee7
Kevin Sylvestre <kevin@ksylvest.com>
2024-08-19 12:58:20 -0700

    Support for hash tools

---
+++ spec/omniai/anthropic/chat/tool_serializer_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+RSpec.describe OmniAI::Anthropic::Chat::ToolSerializer do
+  let(:context) { OmniAI::Anthropic::Chat::CONTEXT }
+
+  describe '#serialize' do
+    subject(:serialize) { tool.serialize(context:) }
+
+    let(:tool) { OmniAI::Tool.new(-> { '...' }, name: 'weather', description: 'Finds the current weather') }
+
+    it { expect(serialize).to eql(name: 'weather', description: 'Finds the current weather') }
+  end
+end
