f3760697b3cd975430a37a403bc9e057b2fa64b1
Kevin Sylvestre <kevin@ksylvest.com>
2024-11-25 14:50:51 -0800

    Support beta flags

--- lib/omniai/anthropic/config.rb
+++ lib/omniai/anthropic/config.rb
@@ -11,20 +11,27 @@ module OmniAI
       #   @return [String, nil] passed as `anthropic-version` if specified
       attr_accessor :version
 
+      # @!attribute [rw] beta
+      #   @return [String, nil] passed as `anthropic-beta` if specified
+      attr_accessor :beta
+
       # @param api_key [String, nil] optional - defaults to `ENV['ANTHROPIC_API_KEY']`
       # @param host [String, nil] optional - defaults to `ENV['ANTHROPIC_HOST'] w/ fallback to `DEFAULT_HOST`
       # @param version [String, nil] optional - defaults to `ENV['ANTHROPIC_VERSION'] w/ fallback to `DEFAULT_VERSION`
+      # @param beta [String, nil] optional - defaults to `ENV['ANTHROPIC_BETA']`
       # @param logger [Logger, nil] optional - defaults to
       # @param timeout [Integer, Hash, nil] optional
       def initialize(
         api_key: ENV.fetch('ANTHROPIC_API_KEY', nil),
         host: ENV.fetch('ANTHROPIC_HOST', DEFAULT_HOST),
         version: ENV.fetch('ANTHROPIC_VERSION', DEFAULT_VERSION),
+        beta: ENV.fetch('ANTHROPIC_BETA', nil),
         logger: nil,
         timeout: nil
       )
         super(api_key:, host:, logger:, timeout:)
         @version = version
+        @beta = beta
         @chat_options[:max_tokens] = 4096
       end
     end
